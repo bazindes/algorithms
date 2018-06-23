969af78780a7aa685b051dbcce8814d366116afe
jingqi.w <wanghe0824@gmail.com>
2018-06-22 20:00:06 -0400

    88

--- java/easy/MergeSortedArray_88.java
+++ java/easy/MergeSortedArray_88.java
@@ -19,12 +19,11 @@ class MergeSortedArray{
     }
 
     public void test(){
-        int[]nums1 = {1, 2, 3, 0, 0, 0,};
+        int[]nums1 = {1, 2, 3, 0, 0, 0};
         int m = 3;
         int[]nums2 = {2, 5, 6};
         int n = 3;
         merge(nums1, m, nums2, n);
         Utils.printArray(nums1);
-
     }
 }
