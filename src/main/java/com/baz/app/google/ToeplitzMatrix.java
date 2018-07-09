82d7811211d4306e52d4c67d297cab6cce2c58a9
jingqi.w <wanghe0824@gmail.com>
2018-07-09 19:45:09 -0400

    463

---
+++ src/main/java/com/baz/app/google/ToeplitzMatrix.java
@@ -0,0 +1,13 @@
+package com.baz.app.google;
+
+public class ToeplitzMatrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix){
+        boolean re = false;
+
+
+
+        return  re;
+    }
+
+}
