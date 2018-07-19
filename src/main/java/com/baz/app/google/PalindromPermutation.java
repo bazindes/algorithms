bdf2611979cb6f3d3e5225cbe997bbb8afe69cb8
jingqi.w <wanghe0824@gmail.com>
2018-07-19 17:35:06 -0400

    266

---
+++ src/main/java/com/baz/app/google/PalindromPermutation.java
@@ -0,0 +1,16 @@
+package com.baz.app.google;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PalindromPermutation {
+    public boolean canPermutePalindrome(String s){
+        if(s == null || "".equals(s)) return false;
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()){
+            if(set.contains(c)) set.remove(c);
+            else set.add(c);
+        }
+        return set.size()<1;
+    }
+}
