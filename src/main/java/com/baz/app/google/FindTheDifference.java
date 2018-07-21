6ecb5e8f07321af3f31c7e97017a41874eceed0e
jingqi.w <wanghe0824@gmail.com>
2018-07-20 22:00:28 -0400

    389

---
+++ src/main/java/com/baz/app/google/FindTheDifference.java
@@ -0,0 +1,21 @@
+package com.baz.app.google;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindTheDifference {
+
+    public char findTheDifference(String s, String t){
+        char re = '0';
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if(!set.contains(t.charAt(i)))
+                re = t.charAt(i);
+        }
+        return re;
+    }
+
+}
