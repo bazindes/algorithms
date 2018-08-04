1ef02fcdb38acfa9bf2ba3b5644b04c0f6ff9265
jingqi.w <wanghe0824@gmail.com>
2018-08-04 11:45:49 -0400

    268

--- src/main/java/com/baz/app/facebook/easy/MissingNumber.java
+++ src/main/java/com/baz/app/facebook/easy/MissingNumber.java
@@ -2,6 +2,7 @@ package com.baz.app.facebook.easy;
 
 public class MissingNumber {
 
+    //O(n) O(n)
     public int missingNumber(int[] nums) {
         int[] ans = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
@@ -15,11 +16,38 @@ public class MissingNumber {
         return -1;
     }
 
+    //O(n) O(1)
+    //Gauss' Formula
+    public int missingNumberGF(int [] nums){
+        int expect = nums.length * (nums.length + 1) / 2;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return expect - sum;
+    }
+
+    /**
+     * O(n) O(1)
+     * Bit manipulation
+     * */
+    public int missingNumberBM(int[] nums){
+        int ans = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= i ^ nums[i];
+        }
+        return ans;
+    }
+
     public void test(){
         int[] nums = {3,0,1};
         System.out.println(missingNumber(nums));
+        System.out.println(missingNumberGF(nums));
+        System.out.println(missingNumberBM(nums));
         nums = new int[]{9,6,4,2,3,5,7,0,1};
         System.out.println(missingNumber(nums));
+        System.out.println(missingNumberGF(nums));
+        System.out.println(missingNumberBM(nums));
     }
 
 }
