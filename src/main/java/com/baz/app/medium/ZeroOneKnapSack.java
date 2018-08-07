028ca6835f7f51ccdfb53c082b1f1c21e4dd1298
jingqi.w <wanghe0824@gmail.com>
2018-08-07 17:11:47 -0400

    01 knapsack

--- src/main/java/com/baz/app/medium/ZeroOneKnapSack.java
+++ src/main/java/com/baz/app/medium/ZeroOneKnapSack.java
@@ -17,12 +17,33 @@ public class ZeroOneKnapSack {
 
     }
 
+    public int knapSackIterative(int[]weights, int[]prices, int totalWeight, int n){
+
+        int[][] ans = new int[n + 1][totalWeight + 1];
+
+        int i, j;
+        for( i = 0; i <= n; i ++){
+            for( j = 0; j <= totalWeight; j ++){
+                if( i == 0 || j == 0){
+                    ans[i][j] = 0;
+                }else if(weights[i - 1] <= j){
+                    ans[i][j] = Math.max(prices[i - 1] + ans[i - 1][j - weights[i-1]] , ans[i - 1][j]);
+                }else {
+                    ans[i][j] = ans[i-1][j];
+                }
+            }
+        }
+
+        return ans[n][totalWeight];
+    }
+
 
     public void test(){
         int[] prices = {60, 100, 120};
         int[] weights = {10, 20, 30};
         int W = 50;
         System.out.println(knapSackFill(weights, prices, W, 3));
+        System.out.println(knapSackIterative(weights, prices, W, 3));
     }
 
 }
