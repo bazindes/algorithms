4fd14c8384da9524a5e33aaec2cb6934c563972d
jingqi.w <wanghe0824@gmail.com>
2018-08-16 19:47:20 -0400

    117

--- src/main/java/com/baz/app/facebook/medium/PopulatingNextRightPointersInEachNodeII.java
+++ src/main/java/com/baz/app/facebook/medium/PopulatingNextRightPointersInEachNodeII.java
@@ -1,7 +1,28 @@
 package com.baz.app.facebook.medium;
 
+import com.baz.app.util.TreeLinkNode;
+
 public class PopulatingNextRightPointersInEachNodeII {
 
-    
+    public void connect(TreeLinkNode root) {
+        while (root != null){
+            TreeLinkNode temp = new TreeLinkNode(0);
+            TreeLinkNode cur = temp;
+
+            while (root != null){
+                if(root.left != null){
+                    cur.next = root.left;
+                    cur = cur.next;
+                }
+                if(root.right != null){
+                    cur.next = root.right;
+                    cur = cur.next;
+                }
+                root = root.next;
+            }
+
+            root = temp.next;
+        }
+    }
 
 }
