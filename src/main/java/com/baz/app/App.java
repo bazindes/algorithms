34b77dadd1b513e79cdf7ab06ca1d8c9b1d0041a
jingqi.w <wanghe0824@gmail.com>
2018-08-16 15:54:48 -0400

    377

--- src/main/java/com/baz/app/App.java
+++ src/main/java/com/baz/app/App.java
@@ -31,8 +31,10 @@ class App{
 
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 
-        SubSetsII subSetsII = context.getBean(SubSetsII.class);
-        subSetsII.test();
+        CombinationSumIV combinationSumIV = context.getBean(CombinationSumIV.class);
+        combinationSumIV.test();
+//        SubSetsII subSetsII = context.getBean(SubSetsII.class);
+//        subSetsII.test();
 //        BrickWall brickWall = context.getBean(BrickWall.class);
 //        brickWall.test();
 //        SimplifyPath simplifyPath = context.getBean(SimplifyPath.class);
