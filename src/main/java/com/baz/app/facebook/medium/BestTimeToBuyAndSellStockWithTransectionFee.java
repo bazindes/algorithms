7bc8ade0f6008727173f2efb834f319106c5c1aa
jingqi.w <wanghe0824@gmail.com>
2018-08-16 23:04:44 -0400

    714

--- src/main/java/com/baz/app/facebook/medium/BestTimeToBuyAndSellStockWithTransectionFee.java
+++ src/main/java/com/baz/app/facebook/medium/BestTimeToBuyAndSellStockWithTransectionFee.java
@@ -2,11 +2,18 @@ package com.baz.app.facebook.medium;
 
 public class BestTimeToBuyAndSellStockWithTransectionFee {
 
+    /**
+     *    DP Solution
+     */
     public int maxProfit(int[] prices, int fee) {
+        // cash: 手头的现金，即总的赚的金额，同时也是未持股时的现金额
         int cash = 0;
+        // hold: 手中有持股时的现金，即总金额减去手中股票的买入价
         int hold = - prices[0];
         for (int i = 0; i < prices.length; i++) {
+            // 如果卖出持股比未持股赚，则卖出
             cash = Math.max(cash, prices[i] + hold - fee);
+            // hold 其实代表买入的最低价
             hold = Math.max(hold, cash - prices[i]);
         }
         return cash;
