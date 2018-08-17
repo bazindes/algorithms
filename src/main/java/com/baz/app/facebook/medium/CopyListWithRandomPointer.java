a85589c560c16495193aaab859a5c32aebbfb567
jingqi.w <wanghe0824@gmail.com>
2018-08-17 00:25:30 -0400

    138

---
+++ src/main/java/com/baz/app/facebook/medium/CopyListWithRandomPointer.java
@@ -0,0 +1,77 @@
+package com.baz.app.facebook.medium;
+
+import com.baz.app.util.RandomListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyListWithRandomPointer {
+
+    //Using a hashmap O(n) O(n)
+    public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        RandomListNode temp = head;
+        while (temp != null){
+            map.put(temp, new RandomListNode(temp.label));
+            temp = temp.next;
+        }
+        RandomListNode temp2 = head;
+        for (RandomListNode cur : map.keySet()){
+            if(cur.next != null){
+                map.get(cur).next = map.get(cur.next);
+            }
+            if(cur.random != null){
+                map.get(cur).random = map.get(cur.random);
+            }
+        }
+
+        return map.get(head);
+    }
+
+
+    //O(n) O(1)
+    public RandomListNode copyRandomListIter(RandomListNode head) {
+        //first round copy every node and inert new node behind the current node and current node.next
+        RandomListNode node = head;
+        RandomListNode next;
+        while (node != null){
+            next = node.next;
+
+            RandomListNode copy = new RandomListNode(node.label);
+            node.next = copy;
+            copy.next = next;
+
+            node = next;
+        }
+
+
+        //second round assign the random
+        node = head;
+        while (node != null){
+            if(node.random != null){
+                node.next.random = node.random.next;
+            }
+            node = node.next.next;
+        }
+
+
+        //Third round exact copy list and restore the original list
+        node = head;
+        RandomListNode forReturn = new RandomListNode(0);
+        RandomListNode iter = forReturn;
+        RandomListNode copy;
+        while (node != null){
+            next = node.next.next;
+
+            copy = node.next;
+            iter.next = copy;
+            iter = copy;
+
+            node.next = next;
+            node = next;
+        }
+
+        return forReturn.next;
+    }
+
+}
---
+++ src/main/java/com/baz/app/util/RandomListNode.java
@@ -0,0 +1,12 @@
+package com.baz.app.util;
+
+public class RandomListNode {
+
+    public int label;
+    public RandomListNode next;
+    public RandomListNode random;
+    public RandomListNode(int label){
+        this.label = label;
+    }
+
+}
