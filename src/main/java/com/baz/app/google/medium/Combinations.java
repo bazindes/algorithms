7795867aa782314c8736a95c2617d71fa408c245
jingqi.w <wanghe0824@gmail.com>
2018-08-19 14:10:13 -0400

    77

--- src/main/java/com/baz/app/google/medium/Combinations.java
+++ src/main/java/com/baz/app/google/medium/Combinations.java
@@ -1,6 +1,7 @@
 package com.baz.app.google.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Combinations {
@@ -30,7 +31,7 @@ public class Combinations {
     }
 
     /***
-     *
+     * fast
      */
     public List<List<Integer>> recursive( int n, int k){
         List<List<Integer>> list = new ArrayList<>();
@@ -45,6 +46,35 @@ public class Combinations {
         }
         list.addAll(recursive(n - 1 , k));
         return list;
+    }
+
+    /**
+     * extremely fast
+     */
+    public List<List<Integer>> combineFast(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (k <= 0 || k > n)
+            return result;
+
+        Integer[] array = new Integer[k];
+        fetchNumber(result, array, 0, 1, n, k);
+
+        return result;
+    }
+
+    private void fetchNumber(List<List<Integer>> result, Integer[] pool, int poolIndex, int from, int to, int count) {
+        if (count == 0) {
+            Integer[] arr = new Integer[pool.length];
+            System.arraycopy(pool, 0, arr, 0, pool.length);
+            result.add(Arrays.asList(arr));
+        } else {
+            int freeCount = (to - from + 1) - (count - 1);
+            for (int i = 0; i < freeCount; i++) {
+                pool[poolIndex] = from + i;
+                fetchNumber(result, pool, poolIndex + 1, from + i + 1, to, count - 1);
+            }
+        }
     }
 
     public void test(){
