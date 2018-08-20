ad52de6dfef73d822043e2a92dd9851d9d48c63a
jingqi.w <wanghe0824@gmail.com>
2018-08-20 14:25:13 -0400

    Google oa

--- src/main/java/com/baz/app/google/medium/MinimumSubSetSum.java
+++ src/main/java/com/baz/app/google/medium/MinimumSubSetSum.java
@@ -2,6 +2,12 @@ package com.baz.app.google.medium;
 
 public class MinimumSubSetSum {
 
+    /**
+     * 给出一个数组，要求两这个数组分为两个部分，分别对两个部分求和，使两个和的绝对差值最小，输出这个最小差值
+     *     eg, [1,2,3,4,5]，可以将它分为[1, 2, 4]和[3, 5]两个部分，对应的和分别为7， 8输出1
+     * @param nums
+     * @return
+     */
 
     public int minCombineSum(int[] nums){
         return helper(0, 0, 0, nums);
