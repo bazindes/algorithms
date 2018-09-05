251682951c6f3a91231302c95a81a2d5af5fa749
jingqi.w <wanghe0824@gmail.com>
2018-09-05 15:23:11 -0400

    46

--- src/main/java/com/baz/app/facebook/medium/Permutations.java
+++ src/main/java/com/baz/app/facebook/medium/Permutations.java
@@ -1,6 +1,9 @@
 package com.baz.app.facebook.medium;
 
+import com.sun.org.apache.regexp.internal.RE;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Permutations {
@@ -22,22 +25,52 @@ public class Permutations {
      */
 
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        LinkedList<List<Integer>> ans = new LinkedList<>();
         if(nums == null || nums.length == 0) return ans;
-        helper(ans, new ArrayList<>() , nums);
+//        helper2(ans, 0, new ArrayList<>() , nums);
+//        helper(ans, new ArrayList<>() , nums);
+        iterative(ans, nums);
         return ans;
+    }
+
+    private void iterative(LinkedList<List<Integer>> ans, int[] nums){
+        ans.add(new ArrayList<>());
+        for (int i : nums){
+            int size = ans.size();
+            for (; size > 0; size --){
+                List<Integer> cur = ans.pollFirst();
+                for (int j = 0; j <= cur.size(); j++) {
+                    List<Integer> temp = new ArrayList<>(cur);
+                    temp.add(j , i);
+                    ans.add(temp);
+                }
+            }
+        }
+    }
+
+    private void helper2(List<List<Integer>> ans, int s, List<Integer> temp, int[] nums){
+        if(nums.length == temp.size()){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i <= temp.size(); i++) {
+            List<Integer> l = new ArrayList<>(temp);
+            l.add(i , nums[s]);
+            helper2(ans, s + 1, l, nums);
+        }
     }
 
     private void helper(List<List<Integer>> ans, List<Integer> temp, int[] nums){
         if(nums.length == temp.size()){
             ans.add(new ArrayList<>(temp));
-        }else {
-            for (int i = 0; i < nums.length; i++) {
-                if(temp.contains(nums[i])) continue;
-                temp.add(nums[i]);
-                helper(ans, temp, nums);
-                temp.remove(temp.size() - 1);
-            }
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            helper(ans, temp, nums);
+            temp.remove(temp.size() - 1);
+
         }
     }
 
