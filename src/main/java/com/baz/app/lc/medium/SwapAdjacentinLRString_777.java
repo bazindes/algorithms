c4610c57b38028cbb697f94b3cc82e3ec415abc1
jingqi.w <wanghe0824@gmail.com>
2018-11-29 16:14:35 -0500

    update

--- src/main/java/com/baz/app/lc/medium/SwapAdjacentinLRString_777.java
+++ src/main/java/com/baz/app/lc/medium/SwapAdjacentinLRString_777.java
@@ -53,32 +53,40 @@ public class SwapAdjacentinLRString_777 implements Google {
 
     // O(n) O(1)
     public boolean conTransFrom(String start, String end){
-        if(! start.replaceAll("X", "").equals(end.replaceAll("X", "")) ) return false;
-
+        char[] S = start.toCharArray();
+        char[] E = end.toCharArray();
         int p1 = 0;
         int p2 = 0;
-
-        while (p1 < start.length() && p2 < end.length()){
+        while (p1 < S.length && p2 < E.length) {
             // get the non-X positions of 2 strings
-            while (p1 < start.length() && start.charAt(p1) == 'X') p1 ++;
-            while (p2 < end.length() && end.charAt(p2) == 'X') p2 ++;
+            while (p1 < S.length && S[p1] == 'X') p1++;
+            while (p2 < E.length && E[p2] == 'X') p2++;
 
-            //if both of the pointers reach the end the strings are transformable
-            if(p1 == start.length() && p2 == end.length()) return true;
-            // if only one of the pointer reach the end they are not transformable
-            if(p1 == start.length() || p2 == end.length()) return false;
-
-            if(start.charAt(p1) != end.charAt(p2)) return false;
-
-            // if the character is 'L', it can only be moved to the left. p1 should be greater or equal to p2.
-            if(start.charAt(p1) == 'L' && p2 > p1) return false;
-            // if the character is 'R', it can only be moved to the right. p2 should be greater or equal to p1.
-            if(start.charAt(p1) == 'R' && p1 > p2) return false;
-
-            p1 ++;
-            p2 ++;
+            if (p1 < S.length && p2 < E.length) {
+                if (S[p1] != E[p2]){
+                    return false;
+                } else {
+                    // if the character is 'L', it can only be moved to the left. p1 should be greater or equal to p2.
+                    if (S[p1] == 'L') { // E[p2] == 'L'
+                        if(p1 < p2){
+                            return false;
+                        } else {
+                            p1++;p2++;
+                        }
+                    } else { // S[p1] == 'R' && E[p2] == 'R', if the character is 'R', it can only be moved to the right. p2 should be greater or equal to p1.
+                        if(p1 > p2){
+                            return false;
+                        } else {
+                            p1++;p2++;
+                        }
+                    }
+                }
+            } else {
+                // if both of the pointers reach the end the strings are transformable
+                // if only one of the pointer reach the end they are not transformable
+                return p1 == p2;
+            }
         }
-
         return true;
     }
 
